474dcb7afad807b2c2bee22f492e96dc2e31cc2e
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2025-07-03 19:13:54 +0530

    Update App.js

--- Component Life Cycle/Coding Practice - 1/Digital Timer App/src/App.js
+++ Component Life Cycle/Coding Practice - 1/Digital Timer App/src/App.js
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import DigitalTimer from './components/DigitalTimer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+import './App.css'
+
+const App = () => <DigitalTimer />
+
+export default App
