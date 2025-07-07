a0b31c5c5f9b99559c2991e1507b6b5f988d9b0a
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2025-07-08 01:13:00 +0530

    Create index.js

---
+++ React Router/Coding Practice - 1/Blog List/src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-detail-container">
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+        alt="profile"
+        className="main-profile"
+      />
+      <h1 className="user-name">Wade Warren</h1>
+      <p className="user-para">Software developer at UK</p>
+    </div>
+  </div>
+)
+
+export default UserInfo
