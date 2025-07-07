c0b6e99320838b2be2514aaaef6fc93110a560b5
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2025-07-08 01:14:01 +0530

    Create index.js

---
+++ React Router/Coding Practice - 1/Blog List/src/components/BlogList/index.js
@@ -0,0 +1,15 @@
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="main-container">
+      {blogsList.map(eachBlog => (
+        <BlogItem eachBlog={eachBlog} key={eachBlog.id} />
+      ))}
+    </ul>
+  )
+}
+export default BlogList
