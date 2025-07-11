965e924b9eaaaddf0fcd8cb66ccce2103727c695
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2025-07-11 20:52:38 +0530

    Create index.js

---
+++ React Router/Routing using React Router Part - 3/src/components/BlogItemDetails/index.js
@@ -0,0 +1,67 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const eachItem = await response.json()
+    const data = {
+      id: eachItem.id,
+      author: eachItem.author,
+      content: eachItem.content,
+      title: eachItem.title,
+      topic: eachItem.topic,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+    }
+    console.log(data)
+    this.setState({blogData: data, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="blog-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          this.renderBlogItemDetails()
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogItemDetails
